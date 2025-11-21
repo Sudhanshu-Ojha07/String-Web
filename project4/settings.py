995f2dc21cec7400e742b47288724da625c64420
Sudhanshu-Ojha07 <sudhanshuojha01643@gmail.com>
2025-11-22 05:25:21 +0530

    Third commmit: change the debug to 0

--- project4/settings.py
+++ project4/settings.py
@@ -24,7 +24,7 @@ if os.path.exists(dotenv_path):
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "unsafe-local-dev-key")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", "0") == "1"
+DEBUG = os.environ.get("DJANGO_DEBUG", "0") == "0"
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
