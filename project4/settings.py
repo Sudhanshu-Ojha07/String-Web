43c13f729373a5de98acf392b151e153eb547147
Sudhanshu-Ojha07 <sudhanshuojha01643@gmail.com>
2025-11-22 03:59:22 +0530

    Second commit: Ready to deploy

--- project4/settings.py
+++ project4/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "unsafe-local-dev-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "0") == "1"
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost").split(",")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 
 # Application definition
